640e631b973ef9c99e162301417589be5725b4fe
Jonathan R. Wallace <jonathan.wallace@gmail.com>
2012-05-21 17:47:31 -0400

    Adds ALL the (basic) functionality

--- lib/resque-lonelyjob.rb
+++ lib/resque-lonelyjob.rb
@@ -3,7 +3,35 @@ require 'resque-lonelyjob/version'
 module Resque
   module Plugins
     module Lonelyjob
-      # Your code goes here...
+      def redis_key(queue)
+        "lonely_job:#{queue}"
+      end
+
+      def can_lock_queue?(queue)
+        Resque.redis.setnx(redis_key(queue), true)
+      end
+
+      def unlock_queue(queue)
+        Resque.redis.del(redis_key(queue))
+      end
+
+      def before_perform(*args)
+        unless can_lock_queue?(@queue)
+          # can't get the lock, so place self at the end of the queue
+          Resque.enqueue(self, *args)
+
+          # and don't perform
+          raise Resque::Job::DontPerform
+        end
+      end
+
+      def after_perform(*args)
+        unlock_queue(@queue)
+      end
+
+      def on_failure(*args)
+        unlock_queue(@queue)
+      end
     end
   end
 end
--- spec/lib/lonelyjob_spec.rb
+++ spec/lib/lonelyjob_spec.rb
@@ -1,7 +1,70 @@
 require 'spec_helper'
 
+  class SerialJob
+    extend Resque::Plugins::Lonelyjob
+    @queue = :serial_work
+
+    def self.perform(*args); end
+  end
+
 describe Resque::Plugins::Lonelyjob do
-  it 'should pass' do
-    true
+  describe ".can_lock_queue?" do
+    before do
+      SerialJob.unlock_queue(:serial_work)
+    end
+
+    it 'can lock a queue' do
+      SerialJob.can_lock_queue?(:serial_work).should be_true
+    end
+
+    it 'cannot lock an already locked queue' do
+      SerialJob.can_lock_queue?(:serial_work).should be_true
+      SerialJob.can_lock_queue?(:serial_work).should be_false
+    end
+  end
+
+  describe ".perform" do
+    it 'should lock and unlock the queue' do
+      job = Resque::Job.new(:serial_work, { 'class' => 'SerialJob', 'args' => %w[account_one job_one] })
+
+      # job is the first SerialJob to run so it can lock the queue and perform
+      SerialJob.should_receive(:can_lock_queue?).and_return(true)
+
+      # but it should also clean up after itself
+      SerialJob.should_receive(:unlock_queue)
+
+      job.perform
+    end
+
+    it 'should clean up lock even with catastrophic job failure' do
+      job = Resque::Job.new(:serial_work, { 'class' => 'SerialJob', 'args' => %w[account_one job_one] })
+
+      # job is the first SerialJob to run so it can lock the queue and perform
+      SerialJob.should_receive(:can_lock_queue?).and_return(true)
+
+      # but we have a catastrophic job failure
+      SerialJob.should_receive(:perform).and_raise(Exception)
+
+      # and still it should clean up after itself
+      SerialJob.should_receive(:unlock_queue)
+
+      # unfortunately, the job will be lost but resque doesn't guarantee jobs
+      # aren't lost
+      -> { job.perform }.should raise_error(Exception)
+    end
+
+    it 'should place self at the end of the queue if unable to acquire the lock' do
+      Resque.size(:serial_work).should == 0
+
+      job = Resque::Job.new(:serial_work, { 'class' => 'SerialJob', 'args' => %w[account_one job_one] })
+
+      SerialJob.should_receive(:can_lock_queue?).and_return(false)
+
+      # perform returns false when DontPerform exception is raised in
+      # before_perform callback
+      job.perform.should be_false
+
+      Resque.size(:serial_work).should == 1
+    end
   end
 end
